d90be982f7fa1a14d58fd03e05affa755e6d021f
Thirsya <thirsya.widya@gmail.com>
2022-11-24 20:41:12 +0700

    UI : Login

---
+++ flutter_api/lib/main.dart
@@ -0,0 +1,114 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: const MyHomePage(),
+      theme: ThemeData(fontFamily: 'Oswald'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key});
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        body: Container(
+      decoration: BoxDecoration(color: Color(0xFFFFEFD6)),
+      width: double.infinity,
+      child: Container(
+        margin: EdgeInsets.all(20),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(
+              'LOGIN',
+              style: TextStyle(fontSize: 30, letterSpacing: 20),
+            ),
+            SizedBox(
+              height: 10,
+            ),
+            TextFormField(
+              //controller: txtEmail,
+              decoration: InputDecoration(
+                contentPadding:
+                    const EdgeInsets.symmetric(vertical: 10, horizontal: 17),
+                filled: true,
+                fillColor: Color(0xFFF2DEBA),
+                labelText: 'Email',
+                labelStyle: const TextStyle(
+                  color: Color(0xFF3A8891),
+                  fontWeight: FontWeight.bold,
+                ),
+                enabledBorder: OutlineInputBorder(
+                  borderSide:
+                      const BorderSide(width: 3, color: Color(0xFF3A8891)),
+                  borderRadius: BorderRadius.circular(15),
+                ),
+                focusedBorder: OutlineInputBorder(
+                  borderSide:
+                      const BorderSide(width: 3, color: Color(0xFF3A8891)),
+                  borderRadius: BorderRadius.circular(15),
+                ),
+              ),
+            ),
+            SizedBox(
+              height: 10,
+            ),
+            TextFormField(
+              //controller: txtEmail,
+              decoration: InputDecoration(
+                contentPadding:
+                    const EdgeInsets.symmetric(vertical: 10, horizontal: 17),
+                filled: true,
+                fillColor: Color(0xFFF2DEBA),
+                labelText: 'Password',
+                labelStyle: const TextStyle(
+                  color: Color(0xFF3A8891),
+                  fontWeight: FontWeight.bold,
+                ),
+                enabledBorder: OutlineInputBorder(
+                  borderSide:
+                      const BorderSide(width: 3, color: Color(0xFF3A8891)),
+                  borderRadius: BorderRadius.circular(15),
+                ),
+                focusedBorder: OutlineInputBorder(
+                  borderSide:
+                      const BorderSide(width: 3, color: Color(0xFF3A8891)),
+                  borderRadius: BorderRadius.circular(15),
+                ),
+              ),
+            ),
+            SizedBox(
+              height: 10,
+            ),
+            Container(
+              width: double.infinity,
+              child: ElevatedButton(
+                  style: ElevatedButton.styleFrom(
+                      backgroundColor: Color(0xFF0E5E6F),
+                      shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(10))),
+                  onPressed: () {},
+                  child: Text('Login')),
+            )
+          ],
+        ),
+      ),
+    ));
+  }
+}

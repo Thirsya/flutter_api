1f35f971db63abf4d3a68a6dde567615ce24d247
Thirsya <thirsya.widya@gmail.com>
2022-11-24 20:59:46 +0700

    UI : Register

--- flutter_api/lib/register.dart
+++ flutter_api/lib/register.dart
@@ -1,30 +1,13 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_api/register.dart';
 
-void main() {
-  runApp(const MyApp());
-}
-
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+class Register extends StatefulWidget {
+  const Register({super.key});
 
   @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      home: const MyHomePage(),
-      theme: ThemeData(fontFamily: 'Oswald'),
-    );
-  }
+  State<Register> createState() => _RegisterState();
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key});
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
+class _RegisterState extends State<Register> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -38,8 +21,35 @@ class _MyHomePageState extends State<MyHomePage> {
               mainAxisAlignment: MainAxisAlignment.center,
               children: [
                 Text(
-                  'LOGIN',
+                  'Register',
                   style: TextStyle(fontSize: 30, letterSpacing: 20),
+                ),
+                SizedBox(
+                  height: 10,
+                ),
+                TextFormField(
+                  //controller: txtEmail,
+                  decoration: InputDecoration(
+                    contentPadding: const EdgeInsets.symmetric(
+                        vertical: 10, horizontal: 17),
+                    filled: true,
+                    fillColor: Color(0xFFF2DEBA),
+                    labelText: 'Name',
+                    labelStyle: const TextStyle(
+                      color: Color(0xFF3A8891),
+                      fontWeight: FontWeight.bold,
+                    ),
+                    enabledBorder: OutlineInputBorder(
+                      borderSide:
+                          const BorderSide(width: 3, color: Color(0xFF3A8891)),
+                      borderRadius: BorderRadius.circular(15),
+                    ),
+                    focusedBorder: OutlineInputBorder(
+                      borderSide:
+                          const BorderSide(width: 3, color: Color(0xFF3A8891)),
+                      borderRadius: BorderRadius.circular(15),
+                    ),
+                  ),
                 ),
                 SizedBox(
                   height: 10,
@@ -105,14 +115,8 @@ class _MyHomePageState extends State<MyHomePage> {
                           backgroundColor: Color(0xFF0E5E6F),
                           shape: RoundedRectangleBorder(
                               borderRadius: BorderRadius.circular(10))),
-                      onPressed: () {
-                        Navigator.push(
-                          context,
-                          MaterialPageRoute(
-                              builder: (context) => const Register()),
-                        );
-                      },
-                      child: Text('Login')),
+                      onPressed: () {},
+                      child: Text('Register')),
                 )
               ],
             ),
